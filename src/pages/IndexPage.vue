bd28e77e92dc71bda0e460ee6c0cc5aa8b92a496
jterblanche <jaun@amtc.com.au>
2023-06-08 05:54:36 +0800

    Add documentation and examples to IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,60 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    {{ stateStore.isomorphicItem }}
   </q-page>
 </template>
 
-<script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+<script lang="ts">
+import { useStateStore } from '@stores/stateStore';
+export default {
+  preFetch(prefetchArgs) {
+    const { currentRoute, previousRoute, redirect, urlPath, publicPath } =
+      prefetchArgs;
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
+    const stateStore = useStateStore();
+
+    /**
+     * IMPORTANT: Read the entire page at https://quasar.dev/quasar-cli-vite/prefetch-feature to understand how this works.
+     *
+     * This runs on the server, not on the client.
+     * No access to this or component instances here.
+     * This is the place to do data fetching and other server side things.
+     * If this is running an async operation, it needs to return a promise.
+     * It is HIGHLY recommended to populate a Pinia store here, which can then be used by client side code.
+     * For example, here we're setting the value of a store property, and in the component we're using the store property.
+     */
+    stateStore.isomorphicItem.value = 'This is an isomorphic item';
+
+    /**
+     * The prefetchArgs object contains a ssrContext property, which is supplied server-side, and can be useful for debugging.
+     */
+    console.log('prefetchArgs', prefetchArgs);
+
+    // In the example below, this prefetch hook can redirect to a login page if the user is not authenticated.
+    // We assume here we already wrote the authentication logic
+    // in the Pinia Store, so take as a high-level example only.
+
+    // if (!stateStore.authenticated) {
+    //   redirect({ path: '/login' });
+    // }
+
+    /**
+     * It is good UX practice to let the user know if something async is happening.
+     * When you add Quasar LoadingBar plugin to your app, Quasar CLI will use it while it runs the preFetch hooks by default.
+     * See https://quasar.dev/quasar-plugins/loading-bar
+     *
+     * Another option is the Quasar Loading plugin.
+     * https://quasar.dev/quasar-plugins/loading
+     */
   },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+};
+</script>
+
+<script setup lang="ts">
+/**
+ * This script runs in a different scope on the client,
+ * we therefore need to instantiate the store again.
+ */
+const stateStore = useStateStore();
+// We can now access the store properties and methods.
 </script>
